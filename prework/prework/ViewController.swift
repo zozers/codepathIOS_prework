03f08cd116b4e3c42547bf808d60c5d47ff7971b
Zoe Offermann <zoffermann15>
2020-01-13 19:14:20 +0100

    added aditional features

--- prework/prework/ViewController.swift
+++ prework/prework/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var textLable: UILabel!
+    
+    @IBOutlet weak var inputField: UITextField!
+    
+    var bgColor: UIColor!
+    var txtColor: UIColor!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        bgColor = view.backgroundColor
+        txtColor = textLable.textColor
     }
 
     @IBAction func didTapButton(_ sender: Any) {
-        print("hello")
+//        print("hello")
         textLable.textColor = UIColor.purple
         
+    }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.green
+    }
+
+    
+    @IBAction func didTapChangeTextButton(_ sender: Any) {
+        
+        if( inputField.text == ""){
+            textLable.text = "No Input!!!"
+        }
+        
+        else{
+            textLable.text = inputField.text
+            inputField.text = ""
+            view.endEditing(true)
+        }
+
+        
+    }
+    
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLable.text = "Hello :)"
+        view.backgroundColor = bgColor
+        textLable.textColor = txtColor
+        inputField.text = ""
     }
     
 }
